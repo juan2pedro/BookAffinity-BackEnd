16e244b371a97f6d197e4c063babd3457d3e1888
carmencami <105524744+carmencami@users.noreply.github.com>
2023-03-27 11:29:55 +0200

    modelo rol y user

---
+++ src/data/models/user.model.ts
@@ -0,0 +1,67 @@
+import { Table, Column, Model } from "sequelize-typescript";
+import { STRING, NUMBER, DATE } from "sequelize";
+import { RolPojo } from "./rol.model";
+import { ForeignKey, BelongsTo } from "sequelize-typescript";
+
+@Table({
+freezeTableName: true,
+schema: "BookAffinity",
+tableName: "user",
+createdAt: true,
+updatedAt: true,
+})
+export class UserPojo extends Model {
+@Column({
+    primaryKey: true,
+    type: NUMBER,
+    field: "id_user",
+    autoIncrement: true,
+})
+id_user: number;
+
+@Column({
+    type: STRING,
+    field: "name",
+})
+name: string;
+
+@Column({
+    type: STRING,
+    field: "pass",
+})
+pass: string;
+
+@Column({
+    type: STRING,
+    field: "email",
+})
+email: string;
+
+@Column({
+    type: NUMBER,
+    field: "status",
+})
+status: number;
+
+@ForeignKey(() => RolPojo)
+@Column({
+    type: NUMBER,
+    field: "id_rol",
+})
+id_rol: number;
+
+@BelongsTo(() => RolPojo)
+rol: RolPojo;
+
+@Column({
+    type: DATE,
+    field: "createdAt",
+})
+createdAt: Date;
+
+@Column({
+    type: DATE,
+    field: "updatedAt",
+})
+updatedAt: Date;
+}
