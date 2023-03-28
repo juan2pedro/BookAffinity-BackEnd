be567a06b406b6398c42235e219341ae31340a2c
carmencami <105524744+carmencami@users.noreply.github.com>
2023-03-28 13:21:02 +0200

    properties

--- src/data/config/chat.db.config.ts
+++ src/data/config/chat.db.config.ts
@@ -2,12 +2,18 @@ import { UserPojo } from './../models/user.model';
 import { Sequelize } from "sequelize-typescript";
 import { ChatPojo } from "../models/chat.model";
 
+var propertiesReader = require('properties-reader');
+var properties = propertiesReader('/src/db_config.properties');
+
+const USERNAME = properties.get('username');
+const PASSWORD = properties.get('password');
+
 export const connect = () => {
     const DB_HOSTNAME = 'localhost'
     const DB_PORT = 5432
     const DB_NAME = 'BookAffinity_db'
-    const DB_USERNAME = 'cami'
-    const DB_PASSWORD = 'Caracola3!'
+    const DB_USERNAME = USERNAME
+    const DB_PASSWORD = PASSWORD
     const DB_SCHEMA = 'Bookaffinity'
     const DB_DIALECT : any = 'postgres'
 
