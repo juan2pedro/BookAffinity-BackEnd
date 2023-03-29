c2bbc510248ef1aae5074fc3867ed26f7508af2f
Gabriel Sánchez <gabionezero@gmail.com>
2023-03-29 13:53:37 +0200

    Fix connect import

--- src/data/repositories/chat.repository.ts
+++ src/data/repositories/chat.repository.ts
@@ -1,6 +1,6 @@
 import { ChatPojo } from './../models/chat.model';
 import { UserPojo } from "../models/user.model";
-import { connect } from "../config/user.db.config";
+import { connect } from "../config/chat.db.config";
 import { Op } from 'sequelize';
 
 export class ChatRepository {
